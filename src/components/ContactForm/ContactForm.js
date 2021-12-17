ac3df67660284ee22381a593be56463fb3061c9b
Yulia Novakovskaya <yonovakovskaya@gmail.com>
2021-12-17 16:34:44 +0200

    Rewrote components ContactForm code using React-hooks

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import styles from "./ContactForm.module.scss";
+
+const ContactForm = ({ onSubmit }) => {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+
+  const handleInput = (event) => {
+    const { name, value } = event.target;
+
+    switch (name) {
+      case "name":
+        setName(value);
+        break;
+
+      case "number":
+        setNumber(value);
+        break;
+
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    onSubmit({ name, number });
+    resetInput();
+  };
+
+  const resetInput = () => {
+    setName("");
+    setNumber("");
+  };
+
+  return (
+    <form className={styles.Form} onSubmit={handleSubmit}>
+      <label className={styles.Label}>
+        Name
+        <input
+          className={styles.Input}
+          id={nanoid()}
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleInput}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </label>
+
+      <label className={styles.Label}>
+        Number
+        <input
+          className={styles.Input}
+          type="tel"
+          name="number"
+          value={number}
+          onChange={handleInput}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </label>
+
+      <button type="submit" className={styles.Button}>
+        Add contact
+      </button>
+    </form>
+  );
+};
+
+export default ContactForm;
